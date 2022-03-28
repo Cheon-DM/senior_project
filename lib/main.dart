bea3fe183d8a3e191bf3b0ad1f3b6976b3c66e36
HaleyHyeseon <88647958+HaleyHyeseon@users.noreply.github.com>
2022-03-28 17:16:51 +0900

    쿠쿠루삥빵뽕

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(const MyApp());
+}
+
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
   return ;
 }
