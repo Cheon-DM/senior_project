513be2d8805f44e9e6f928b4d15aa3f37321b725
HwonPark <ioio6085@naver.com>
2022-05-03 18:50:46 +0900

    지도

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
+import 'package:flutter_kakao_map/flutter_kakao_map.dart';
+import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
 import 'package:get/get.dart';
 import 'package:senior_project/HS/landingpage.dart';
 
@@ -7,10 +9,49 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: LandingPage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyAppOne(),
     );
   }
 }
 
-// 왜 안가 ?
+class MyAppOne extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyAppOne> {
+  late KakaoMapController mapController;
+  MapPoint _visibleRegion = MapPoint(37.5087553, 127.0632877);
+  CameraPosition _kInitialPosition =
+  CameraPosition(target: MapPoint(37.5087553, 127.0632877), zoom: 5);
+
+  void onMapCreated(KakaoMapController controller) async {
+    final MapPoint visibleRegion = await controller.getMapCenterPoint();
+    setState(() {
+      mapController = controller;
+      _visibleRegion = visibleRegion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter KakaoMap example')),
+      body: Column(
+        children: [
+          Center(
+              child: SizedBox(
+                  width: 300.0,
+                  height: 200.0,
+                  child: KakaoMap(
+                      onMapCreated: onMapCreated,
+                      initialCameraPosition: _kInitialPosition)))
+        ],
+      ),
+    );
+  }
+}
+
+// 왜 안가 ?*/
