e115a15564d83ac38e1899c25570b80b0f1a21cf
HaleyHyeseon <88647958+HaleyHyeseon@users.noreply.github.com>
2022-05-21 20:19:27 +0900

    ui디자인~~~ 에셋 다들 잘 넣어주세용

--- lib/HS/landingpage.dart
+++ lib/HS/landingpage.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:senior_project/HS/mainpage.dart';
+import 'mainpage.dart';
 
 class LandingPage extends StatefulWidget {
   @override
@@ -12,20 +12,23 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage>{
   @override
   void initState() {
-    Timer(Duration(seconds: 3), (){
-      Get.offAll(MainPage());
+    Timer(Duration(seconds: 3), () {
+      Get.offAll(()=>MainPage());
     });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.blue,
-        child: Center(child: Text("여기로 가")),
-        ),
-      );
+    return MaterialApp(
+      home: Container(
+          color: const Color(0xff6157de),
+          child: FractionallySizedBox(
+            widthFactor: 0.35,
+            heightFactor: 0.35,
+            child: Image.asset('assets/logo_white.png'),
+          ),
+    ),
+    );
   }
 }
-
--- lib/HS/mainpage.dart
+++ lib/HS/mainpage.dart
@@ -1,10 +1,15 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_navigation/src/routes/get_transition_mixin.dart';
 import 'package:senior_project/HS/action_guide.dart';
 import 'package:senior_project/HS/disaster_sms.dart';
-import 'package:senior_project/HS/location_sharing.dart';
-import 'package:senior_project/HS/loginPage.dart';
-import 'package:senior_project/HS/myPage.dart';
+
+import 'location_sharing.dart';
+import 'loginPage.dart';
+
 
 class MainPage extends StatefulWidget {
   @override
@@ -12,60 +17,295 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('메인페이지'),),
-      body: Center(
-        child: Column(
-          children: [
-            OutlinedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) {
-                        return PreLoginPage();
-                      }));
-                },
-                child: Text("내 정보")
-            ),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: ElevatedButton(
-                  onPressed: () {
-
-                  },
-                  child: Text("내 주변 대피소")
+    return MaterialApp(
+      home: Container(
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color(0xff6157DE),
+              elevation: 0,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset('assets/logo_white.png',
+                  fit: BoxFit.contain,
+                  height: 20,),
+                ],
               ),
+              actions: <Widget>[
+                IconButton(
+                    onPressed: (){
+                      Get.to(PreLoginPage());
+                    },
+                    icon: Icon(
+                      Icons.perm_contact_calendar_outlined,
+                      color: Colors.white,
+                    ),
+                ),
+              ],
             ),
-            OutlinedButton(
-                onPressed: () {
-                  Get.to(LocationSharing());
-                },
-                child: Text("위치공유")
-            ),
-            OutlinedButton(
-                onPressed: () {
-                  Get.to(ActionGuide());
-                },
-                child: Text("행동지침")
-            ),
-            OutlinedButton(
-                onPressed: () {
-                  Get.to(DisasterSMS());
-                },
-                child: Text("재난문자")
-            ),
-            OutlinedButton(
-                onPressed: () {
+              resizeToAvoidBottomInset: false, //키보드로 밀려올라감 방지
+              backgroundColor: Colors.grey[200],
 
-                },
-                child: Text("빈칸><")
-            ),
-          ],
-        )
-      )
+              body: SafeArea(
+                child: Column(
+                  //버튼 4개 전체적으로 정렬
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
 
+                  children: <Widget>[
+                    SizedBox(height: 0,),
+                    Container(
+                      child: Column(
+                        //텍스트 위치 정렬
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          CupertinoButton(
+                              child: Text(
+                                "내 주변 대피소",
+                                style: TextStyle(
+                                  fontFamily: 'Leferi',
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            onPressed: (){
+                              Get.to(LocationSharing());
+                            },
+                          ),
+                          SizedBox(height: 0,),
+                          Text(
+                            "나와 가장 가까운 대피소",
+                            style: TextStyle(
+                              fontFamily: 'Leferi',
+                              color: Colors.grey[300],
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 50,),
+                        ],
+                      ),
+
+                      //보라색(내 주변 대피소)
+                      height: MediaQuery.of(context).size.height*0.4,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(left : 0, right: 0, bottom: 30),
+                      decoration: BoxDecoration(
+                          borderRadius:  const BorderRadius.only(
+                            bottomRight: Radius.circular(0),
+                            bottomLeft: Radius.circular(100),
+                          ),
+                          color: const Color(0xff6157DE),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              blurRadius: 10,
+                              spreadRadius: 5,
+                              offset: Offset(0,5),
+                            ),
+
+                          ]
+                      ),
+                    ),
+
+                    //행동지침
+                    Container(
+                      child: Row(
+                        //박스 내부 정렬
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CupertinoButton(
+                            onPressed: () {
+                              Get.to(ActionGuide());
+                            },
+                            child: Row(children: <Widget>[
+                              Container(
+                                child: Container(
+                                  child: Icon(
+                                    Icons.menu_book_rounded,
+                                    color: Colors.black,
+                                    size: MediaQuery.of(context).size.height*0.12*0.4,
+                                  ),
+                                  margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                ),
+                              ),
+                              Text(
+                                "행동지침",
+                                style: TextStyle(
+                                  fontFamily: 'Leferi',
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          ),
+                        ],
+                      ),
+                      //카드 디자인
+                      height: MediaQuery.of(context).size.height*0.12,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(left : 100, right: 0, top : 25,),
+                      decoration: BoxDecoration(
+                          borderRadius:  const BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            bottomLeft: Radius.circular(30),
+                          ),
+                          color: Colors.grey[200],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: Offset(4,4),
+                            ),
+                            BoxShadow(
+                              color: Colors.white,
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: Offset(-4,-4),
+                            )
+                          ]
+                      ),
+                    ),
+
+                    //위치공유
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CupertinoButton(
+                            onPressed: () {
+                              Get.to(LocationSharing());
+                            },
+                            child: Row(children: <Widget>[
+                              Container(
+                                child:
+                                Container(
+                                  child: Icon(
+                                    Icons.add_location_alt_outlined,
+                                    color: Colors.black,
+                                    size: MediaQuery.of(context).size.height*0.12*0.4,
+                                  ),
+                                  margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                ),
+                              ),
+                              Text(
+                                "위치공유",
+                                style: TextStyle(
+                                  fontFamily: 'Leferi',
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      height: MediaQuery.of(context).size.height*0.12,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(left : 100, right: 0, top : 15),
+                      decoration: BoxDecoration(
+                          borderRadius:  const BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            bottomLeft: Radius.circular(30),
+                          ),
+                          color: Colors.grey[200],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: Offset(4,4),
+                            ),
+                            BoxShadow(
+                              color: Colors.white,
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: Offset(-4,-4),
+                            )
+                          ]
+                      ),
+                    ),
+
+                    //재난문자
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CupertinoButton(
+                            onPressed: () {
+                              Get.to(DisasterSMS());
+                            },
+                            child: Row(children: <Widget>[
+                              Container(
+                                child:
+                                Container(
+                                  child: Icon(
+                                    Icons.sms_failed_outlined,
+                                    color: Colors.black,
+                                    size: MediaQuery.of(context).size.height*0.12*0.4,
+                                  ),
+                                  margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                ),
+                              ),
+                              Text(
+                                "재난문자",
+                                style: TextStyle(
+                                  fontFamily: 'Leferi',
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      height: MediaQuery.of(context).size.height*0.12,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(left : 100, right: 0, top : 15),
+                      decoration: BoxDecoration(
+                          borderRadius:  const BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            bottomLeft: Radius.circular(30),
+                          ),
+                          color: Colors.grey[200],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: Offset(4,4),
+                            ),
+                            BoxShadow(
+                              color: Colors.white,
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: Offset(-4,-4),
+                            )
+                          ]
+                      ),
+                    ),
+                  ],
+                ),
+              )
+          )
+
+
+      ),
     );
   }
 }
