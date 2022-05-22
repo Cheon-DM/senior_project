7de25858e9a726b864bbc3923ac11f02986291b1
HaleyHyeseon <88647958+HaleyHyeseon@users.noreply.github.com>
2022-05-22 20:20:32 +0900

    행동지침 디자인 완료(내용은 방학에)

--- lib/HS/action_guide.dart
+++ lib/HS/action_guide.dart
@@ -13,53 +13,140 @@ class _ActionGuideState extends State<ActionGuide> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: '행동지침1',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: '행동지침1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xff6157DE),
+          elevation: 5,
+          title: Text(
+            "행동지침",
+            style: TextStyle(
+              fontFamily: 'Leferi',
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              // Get.to(MainPage());
+              Get.offAll(() => MainPage());
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+          ),
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: const Color(0xff6157DE),
-            elevation: 0,
-            title: Text(
-              "행동지침",
-              style: TextStyle(
-                fontFamily: 'Leferi',
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            leading: IconButton(
-              onPressed: (){
-                // Get.to(MainPage());
-                Get.offAll(() => MainPage());
-              },
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          drawer: Drawer(
-            child: ListView(
-              children: <Widget>[
-                ExpansionTile(
-                  title: Text('정보1'),
-                  children: <Widget>[
-                    ListTile(
-                      title: Text('정보1.1'),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: Text('정보1.2'),
-                      onTap: () {},
-                    ),
-                  ],
+        body: Padding(
+          padding: EdgeInsets.all(0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child:
+                Container(
+                  color: Color(0xff6157de),
+                  child: ListView(
+                    children: <Widget>[
+                      ExpansionTile(
+                        title: Text(
+                          "메뉴1",
+                          style: TextStyle(
+                            fontFamily: 'Leferi',
+                            fontSize: 15,
+                          ),
+                        ),
+                        backgroundColor: Colors.grey[200],
+                        collapsedBackgroundColor: Color(0xff6157de),
+                        collapsedTextColor: Colors.white,
+                        textColor: Colors.black,
+                        iconColor: Colors.black,
+                        collapsedIconColor: Colors.white,
+
+                        children: <Widget>[
+                          ListTile(
+                            title: Text(
+                              "메뉴1.1",
+                              style: TextStyle(
+                                fontFamily: 'Leferi',
+                                fontSize: 13,
+                              ),
+                            ),
+                            textColor: Colors.black,
+                            onTap: () {},
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ],
-            ),
+                flex: 1,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "메뉴1.1 value",
+                        style: TextStyle(
+                          fontFamily: 'Leferi',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        height: 15,
+                      ),
+                      Text(
+                        "상황별 행동지침(국민재난안전포털 웹크롤링)",
+                        style: TextStyle(
+                          fontFamily: 'Leferi',
+                          fontSize: 13,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                flex: 2,
+              ),
+
+              /*
+                                    ListView(
+                    children: <Widget>[
+                      ExpansionTile(
+                        title: Text('정보1'),
+                        children: <Widget>[
+                          ListTile(
+                            title: Text('정보1.1'),
+                            onTap: () {},
+                          ),
+                          ListTile(
+                            title: Text('정보1.2'),
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  ListView(
+                    children: <Widget>[
+
+                    ],
+                  )
+
+                   */
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
