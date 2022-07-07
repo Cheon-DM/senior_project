621691760b51fbe2f0f4e755b12112ec6d788863
HwonPark <ioio6085@naver.com>
2022-07-07 18:18:55 +0900

    지도

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:senior_project/HS/landingpage.dart';
+import 'package:senior_project/HW/addFriend.dart';
 import 'DM/Auth.dart';
 import 'DM/findShelter.dart';
 import 'DM/message.dart';
 import 'HS/mainpage.dart';
 
+
 void main() async {
+
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   authFirebase();
   runApp(MaterialApp(
-      home: LandingPage()));
+      home: AddFriend()));
 }
