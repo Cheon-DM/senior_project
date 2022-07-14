462324e9f149e39f2e7bfbc92289513a725c3d28
HaleyHyeseon <88647958+HaleyHyeseon@users.noreply.github.com>
2022-07-14 19:56:16 +0900

    행동지침 수정중(아직 오류남*^^*)

--- lib/HS/show_guide.dart
+++ lib/HS/show_guide.dart
@@ -1,50 +1,36 @@
-import 'dart:ui';
-
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:senior_project/HW/login.dart';
-import 'action_guide2.dart';
+import 'package:flutter/services.dart' as rootBundle;
+import 'package:senior_project/HS/guideLineModel.dart';
 
-import 'dart:async';
-import 'package:flutter/services.dart' show rootBundle;
+import 'action_guide2.dart';
 
 
 class ShowGuide extends StatefulWidget {
+
   @override
   _ShowGuideState createState() => _ShowGuideState();
 }
 
-
 class _ShowGuideState extends State<ShowGuide> {
   final ScrollController _scrollController = ScrollController();
 
-  String data = 'empty';
-  fetchFileData() async {
-    String responseText;
-    responseText = await rootBundle.loadString('assets/guide/guide1_1.txt');
-
-    setState(() {
-      data = responseText;
-    });
-  }
-
   @override
   void initState() {
-    fetchFileData();
+    ReadJsonData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'show-guide',
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: const Color(0xff6157DE),
           elevation: 5,
           title: Text(
-            "행동지침",
+            "행동지침_상세",
             style: TextStyle(
               fontFamily: 'Leferi',
               color: Colors.white,
@@ -54,9 +40,10 @@ class _ShowGuideState extends State<ShowGuide> {
           ),
           leading: IconButton(
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return ActionGuide2();
-              }));
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) {
+                    return ActionGuide2();
+                  }));
             },
             icon: Icon(
               Icons.arrow_back,
@@ -64,30 +51,63 @@ class _ShowGuideState extends State<ShowGuide> {
             ),
           ),
         ),
-
-
-        body: Scrollbar(
-          controller: _scrollController,
-         isAlwaysShown: true,
-         thickness: 10,
-         child: ListView(
-           controller: _scrollController,
-           children: [
-             Expanded(
-               child: Center(
-                     child: Padding(
-                       padding: const EdgeInsets.all(8.0),
-                       child: Text(
-                         data,
-                         style: TextStyle(fontSize: 18),
-                       ),
-                     ),
-                   ),
-               ),
-           ],
-         ),
-        ),
-      ),
+        body: FutureBuilder(
+          future: ReadJsonData(),
+          builder: (context, data) {
+            if(data.hasError){
+              print('${"응 데이터에러야 ㅋㅋ"}');
+              return Center(child: Text("${data.error}"));
+            } else if(data.hasData) {
+              var items = data.data as List<GuideLineModel>;
+              print('${"우와! 데이터가 있다!"}');
+              return Container(
+                child: Text(
+                  items.toString(),
+                ),
+              );
+            }else{
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        )
     );
+
+  }
+
+
+  /* 제대로 잘 돌아가는 원본
+  Future<List<GuideLineModel>>ReadJsonData() async {
+    final jsonData = await rootBundle.rootBundle.loadString('assets/guide/guide1_1.json');
+    final list = json.decode(jsonData) as List<dynamic>;
+
+    print('${"으아아아"}');
+    print('${list[0]}');
+    print('${"아아아악"}');
+    print('${list[1]}');
+    return list.map((e) => GuideLineModel.fromJson(e)).toList();
+  }
+   */
+
+  // 자료형 문제 좀 고쳐보려고 건드리는 부분
+  Future<String>ReadJsonData() async {
+    final jsonData = await rootBundle.rootBundle.loadString('assets/guide/guide1_1.json');
+    final list = json.decode(jsonData) as List<dynamic>;
+    var item = list[0].toString();
+    print('${"으아아아"}');
+    print('${list[0]}');
+    print('${"아아아악"}');
+    print('${list[1]}');
+    print('${"출력값"}');
+    print('${item}');
+    print('${"리턴값"}');
+    return item;
+
+    //fromJson : map구조에서 새로운 user객체를 생성하는 생성자
+    //toJson : user객체를 map구조로 변환하는 함수
+
+
+
+    //print('${list.map((e) => GuideLineModel.fromJson(e)).toList()}');
+
   }
 }
