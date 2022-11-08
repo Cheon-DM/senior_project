c2245adac511a48bafee03782020ee36c7b99e35
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-08 17:22:43 +0900

    style(friends location): 불필요코드 정리

--- lib/friendsManage/find_friends_map.dart
+++ lib/friendsManage/find_friends_map.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:kakaomap_webview/kakaomap_webview.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-import '../mainpage.dart';
 import '../provider/LocateData.dart';
 
 const String kakaoMapKey = '9e9e53f5a50038a1fdb31333c3afc1d2';
@@ -80,11 +79,9 @@ class _FindFriendLocation extends State<FindFriendLocation> {
                     stream: locate(),
                     builder: (context, snapshot) {
                       if (isLoading){
-                        print("_isLoading = true");
                         return const CircularProgressIndicator();
                       }
                       else {
-                        print("_isLoading = false");
                         return KakaoMapView(
                             width: size.width,
                             height: size.height / 2,
@@ -98,69 +95,54 @@ class _FindFriendLocation extends State<FindFriendLocation> {
                             },
                             zoomLevel: 2,
                             customScript: '''
-    var markers = [];
-    var friendImageURL = 'http://t1.daumcdn.net/localimg/localimages/07/2012/img/marker_normal.png';
-    
-    var flat = ${locateProvider.friend_lat};
-    var flng = ${locateProvider.friend_lng};
-    var fname = ${locateProvider.friend_name};
-    console.log(fname);
-
-    function addFriendMarker(marker, image) {
-      marker.setMap(map);
-      markers.push(marker);
-    }
-
-    function createMarkerImage(src, size, options) {
-      var markerImage = new kakao.maps.MarkerImage(src, size, options);
-      return markerImage;            
-    }
-    
-    function clickMarker(marker, iwContent, iwRemoveable) {
-      var infowindow = new kakao.maps.InfoWindow({
-        content : iwContent,
-        removable : iwRemoveable
-      });
+      var markers = [];
+      var friendImageURL = 'http://t1.daumcdn.net/localimg/localimages/07/2012/img/marker_normal.png';
       
-      kakao.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
-      });
-    }
-    
-    var customStyle = document.createElement("style");
-    document.head.appendChild(customStyle);
-    customStyle.sheet.insertRule(".fname {text-align : center; width: 100px;box-shadow: 3px 4px 0px 0px #1564ad;	background-color:transparent;	border-radius:6px;	border:1px solid #337bc4;	display:inline-block;	cursor:pointer;	color:#3e3670;	font-family:sans-serif;	font-size:16px;	padding:16px 30px;	text-decoration:none;	text-shadow:0px 1px 0px #528ecc;}");
-    customStyle.sheet.insertRule(".fname:hover {background-color:transparent;}");
-    customStyle.sheet.insertRule(".fname:active {position:relative;}");
-    
-    for(let i = 0 ; i < flat.length ; i++){
-      var imageSize = new kakao.maps.Size(35, 45);
-      var imageOptions = {  
-          spriteOrigin: new kakao.maps.Point(0, 0 + i * 50),    
-          spriteSize: new kakao.maps.Size(644, 946)  
-      };
-    
-      var friendMarkerImage = createMarkerImage(friendImageURL, imageSize, imageOptions);
-      var marker = new kakao.maps.Marker({position: new kakao.maps.LatLng(flat[i], flng[i]), image: friendMarkerImage, clickable: true});
-      var iwContent = '<div class="fname">' + fname[i] + '</div>';
+      var flat = ${locateProvider.friend_lat};
+      var flng = ${locateProvider.friend_lng};
+      var fname = ${locateProvider.friend_name};
+  
+      function addFriendMarker(marker, image) {
+        marker.setMap(map);
+        markers.push(marker);
+      }
+  
+      function createMarkerImage(src, size, options) {
+        var markerImage = new kakao.maps.MarkerImage(src, size, options);
+        return markerImage;            
+      }
       
-      addFriendMarker(marker, friendMarkerImage);
-      clickMarker(marker, iwContent, true);
-    }
+      function clickMarker(marker, iwContent, iwRemoveable) {
+        var infowindow = new kakao.maps.InfoWindow({
+          content : iwContent,
+          removable : iwRemoveable
+        });
+        
+        kakao.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map, marker);
+        });
+      }
     
-    var info = document.querySelectorAll('.info');
-    console.log(info);
-    info.forEach(function(e) {
-        var w = e.offsetWidth + 10;
-        var ml = w/2;
-        e.parentElement.style.top = "82px";
-        e.parentElement.style.left = "50%";
-        e.parentElement.style.marginLeft = -ml+"px";
-        e.parentElement.style.width = w+"px";
-        e.parentElement.previousSibling.style.display = "none";
-        e.parentElement.parentElement.style.border = "0px";
-        e.parentElement.parentElement.style.background = "#65c9be";
-    });
+      var customStyle = document.createElement("style");
+      document.head.appendChild(customStyle);
+      customStyle.sheet.insertRule(".fname {text-align : center; width: 100px;box-shadow: 3px 4px 0px 0px #1564ad;	background-color:transparent;	border-radius:6px;	border:1px solid #337bc4;	display:inline-block;	cursor:pointer;	color:#3e3670;	font-family:sans-serif;	font-size:16px;	padding:16px 30px;	text-decoration:none;	text-shadow:0px 1px 0px #528ecc;}");
+      customStyle.sheet.insertRule(".fname:hover {background-color:transparent;}");
+      customStyle.sheet.insertRule(".fname:active {position:relative;}");
+    
+      for(let i = 0 ; i < flat.length ; i++){
+        var imageSize = new kakao.maps.Size(35, 45);
+        var imageOptions = {  
+            spriteOrigin: new kakao.maps.Point(0, 0 + i * 50),    
+            spriteSize: new kakao.maps.Size(644, 946)  
+        };
+      
+        var friendMarkerImage = createMarkerImage(friendImageURL, imageSize, imageOptions);
+        var marker = new kakao.maps.Marker({position: new kakao.maps.LatLng(flat[i], flng[i]), image: friendMarkerImage, clickable: true});
+        var iwContent = '<div class="fname">' + fname[i] + '</div>';
+        
+        addFriendMarker(marker, friendMarkerImage);
+        clickMarker(marker, iwContent, true);
+      }
 
 		  const zoomControl = new kakao.maps.ZoomControl();
       map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
@@ -225,6 +207,7 @@ class _FindFriendLocation extends State<FindFriendLocation> {
               InkWell(
                 onTap: () async {
                   locateProvider.locateMe();
+
                   await _mapController.clearCache();
                   debugPrint('[refresh] done');
                 },
