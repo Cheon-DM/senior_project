bc56261f204b2f1e76fe9dbc4832cb9dc04bcff5
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-08 16:55:28 +0900

    style(guide): 쓰이지 않는 코드 정리
    쓰이지 않는 코드 정리
    - 불필요한 import파일 정리
    - 필요없는 코드 정리

--- lib/guideline/action_guide.dart
+++ lib/guideline/action_guide.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../mainpage.dart';
 import '../provider/GuideData.dart';
 
 class ActionGuide extends StatefulWidget {
